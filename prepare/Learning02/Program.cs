8306d8f6989f58546286ec28881cc3483abc83c5
wesley <someonelse2009@gmail.com>
2025-09-29 18:55:28 -0500

    completed learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "poopie.co";
+        job1._startYear = 2020;
+        job1._endYear = 2222;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Eater";
+        job2._company = "murlpe.co";
+        job2._startYear = 220;
+        job2._endYear = 222;
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Jon";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
