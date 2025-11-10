c140eba62338ebc1717547fedfebe49dce10926b
wesley <someonelse2009@gmail.com>
2025-11-10 13:52:41 -0600

    completed learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square david = new Square("Red", 4.0);
+        Rectangle sarah = new Rectangle("Blue", 3.0, 5.0);
+        Circle michael = new Circle("Green", 2.5);
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(david);
+        shapes.Add(sarah);
+        shapes.Add(michael);
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
